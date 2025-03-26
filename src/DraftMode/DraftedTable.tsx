d010a9ad5d91766bf753e9c6f10a18e94583cb06
TheKlegacy <domininkkingdiepie@gmail.com>
2025-03-25 21:40:10 -0500

    finish draft function and fix drags

--- src/DraftMode/DraftedTable.tsx
+++ src/DraftMode/DraftedTable.tsx
@@ -3,27 +3,27 @@ import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { ColDef, ValueGetterParams, RowDragEvent } from "ag-grid-community";
-import { useAtomValue, useSetAtom } from 'jotai';
-import { filteredPlayersAtom, getCurrentBoard, updateBoard, currentPlayersAtom } from "../Atoms";
+import { useAtomValue } from 'jotai';
+import { draftedBoard } from "../Atoms";
 import { PlayerNameCellRenderer } from "../shared/PlayerNameCellRenderer";
 
 export const DraftedTable: React.FC = () => {
-    const rowData = useAtomValue(filteredPlayersAtom);
-    const currentPlayers = useAtomValue(currentPlayersAtom);
-    const board = useAtomValue(getCurrentBoard);
-    const setBoard = useSetAtom(updateBoard);
+    const rowData = useAtomValue(draftedBoard).Players;
 
     const defaultColDef = useMemo(() => { return { sortable: false, }; }, []);    
 
-    const [columnDefs] = useState<ColDef<Player>[]>([
+    const [columnDefs] = useState<ColDef<PlayerDrafted>[]>([
         {
-            headerName: "#",
-            valueGetter: "node.rowIndex + 1",
+            headerName: "Pick",
+            valueGetter: (params: ValueGetterParams<PlayerDrafted>) => {
+                const pick = params.data?.Pick ?? 0;
+                return `${Math.floor((pick - 1) / 12) + 1}.${((pick - 1) % 12) + 1}`
+            },
             width: 70
         },
         {
             headerName: "Pos",
-            valueGetter: (params: ValueGetterParams<Player>) =>
+            valueGetter: (params: ValueGetterParams<PlayerDrafted>) =>
                 params.data?.position,
             width: 80
         },
@@ -33,34 +33,10 @@ export const DraftedTable: React.FC = () => {
         },
     ]);
 
-    const onRowDragEnd = (event: RowDragEvent) => {
-        const draggedData = event.node.data;
-        const updatedData = [...currentPlayers];
-
-        const draggedIndex = updatedData.findIndex(
-            (item) => item.player_id === draggedData.player_id
-        );
-
-        if (draggedIndex !== -1) {
-            const newIndex = updatedData.findIndex(
-                (p) => p.player_id === rowData[event.overIndex].player_id
-            );
-
-            updatedData.splice(draggedIndex, 1);
-            updatedData.splice(newIndex, 0, draggedData);
-
-            updatedData.forEach((player, index) => {
-                player.rank = index + 1;
-            });
-
-            setBoard({ ...board, Players: updatedData });
-        }
-    };
-
     return (
         <div
             className="ag-theme-alpine-dark"
-            style={{ height: "75vh", width: "450px", margin: "2em" }}
+            style={{ height: "75vh", width: "420px", margin: "2em" }}
         >
             <AgGridReact
                 rowData={rowData.slice(0, 500)}
@@ -69,7 +45,6 @@ export const DraftedTable: React.FC = () => {
                 animateRows={true}
                 rowDragMultiRow={true}
                 rowSelection={{ mode: "multiRow", headerCheckbox: false }}
-                onRowDragEnd={onRowDragEnd}
                 rowDragEntireRow={true}
                 rowDragManaged={true}
                 suppressScrollOnNewData={true}
--- src/DraftMode/UndraftedTable.tsx
+++ src/DraftMode/UndraftedTable.tsx
@@ -4,12 +4,14 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { ColDef, ValueGetterParams, RowDragEvent } from "ag-grid-community";
 import { useAtom, useAtomValue, useSetAtom } from 'jotai';
-import { updateBoard, filteredDraftBoardPlayersAtom, draftBoard } from "../Atoms";
+import { draftPlayer, filteredDraftBoardPlayersAtom, draftBoard } from "../Atoms";
 import { PlayerNameCellRenderer } from "../shared/PlayerNameCellRenderer";
+import { Button } from "@mui/material";
 
 export const UndraftedTable: React.FC = () => {
     const rowData = useAtomValue(filteredDraftBoardPlayersAtom);
     const [board, setBoard] = useAtom(draftBoard);
+    const draft = useSetAtom(draftPlayer);
 
     const defaultColDef = useMemo(() => { return { sortable: false, }; }, []);    
 
@@ -58,37 +60,38 @@ export const UndraftedTable: React.FC = () => {
             valueGetter: (params: ValueGetterParams<Player>) =>
                 params.data?.college,
             width: 160
+        },
+        {
+            headerName: "Actions",
+            cellRenderer: (params: ValueGetterParams<Player>) => 
+                (<Button onClick={() => draft(params.data!)} variant="text">Draft</Button>),
+            width: 100
         }
     ]);
 
-    const onRowDragEnd = (event: RowDragEvent) => {
-        const draggedData = event.node.data;
-        const updatedData = [...board.Players];
+    const onRowDragEnd = (event: RowDragEvent<Player>) => {
+        const nodes = event.nodes;
+        const draggedData = nodes.map(node => node.data!);
+        let updatedData = [...board!.Players];
 
-        const draggedIndex = updatedData.findIndex(
-            (item) => item.player_id === draggedData.player_id
+        const newIndex = updatedData.findIndex(
+            (p) => p.player_id === rowData[event.overIndex].player_id
         );
 
-        if (draggedIndex !== -1) {
-            const newIndex = updatedData.findIndex(
-                (p) => p.player_id === rowData[event.overIndex].player_id
-            );
+        draggedData.forEach(p => updatedData = updatedData.filter(p2 => p?.player_id !== p2.player_id));
+        updatedData.splice(newIndex, 0, ...draggedData);
 
-            updatedData.splice(draggedIndex, 1);
-            updatedData.splice(newIndex, 0, draggedData);
-
-            updatedData.forEach((player, index) => {
-                player.rank = index + 1;
-            });
-
-            setBoard({ ...board, Players: updatedData });
-        }
+        updatedData.forEach((player, index) => {
+            player.rank = index + 1;
+        }); 
+        
+        setBoard({ ...board!, Players: updatedData });
     };
 
     return (
             <div
                 className="ag-theme-alpine-dark"
-                style={{ height: "75vh", width: "860px", margin: "2em" }}
+                style={{ height: "75vh", width: "965px", margin: "2em" }}
             >
                 <AgGridReact
                     rowData={rowData.slice(0, 500)}
