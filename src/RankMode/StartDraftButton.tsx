5415fe8b692bb62e36df371b53631110b7491084
TheKLegacy <dominikkingdiepie@gmail.com>
2025-03-25 21:51:40 -0500

    Fix draft board

--- src/RankMode/StartDraftButton.tsx
+++ src/RankMode/StartDraftButton.tsx
@@ -1,16 +1,17 @@
 import { Button } from "@mui/material";
 import React from "react";
-import { displayMode, draftBoard, getCurrentBoard } from "../Atoms";
-import { useAtomValue, useSetAtom } from "jotai";
+import { displayMode, draftBoard, draftedBoard, getCurrentBoard } from "../Atoms";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
 
 export const StartDraftButton: React.FC = () => {
     const setDisplayMode = useSetAtom(displayMode);
     const setDraftBoardAtom = useSetAtom(draftBoard);
+    const setdraftedBoardAtom = useSetAtom(draftedBoard);
     const currentBoard = useAtomValue(getCurrentBoard);
 
     return (
         <>
-            <Button variant="outlined" size="large" onClick={() => (setDisplayMode("draft"),  setDraftBoardAtom(currentBoard))}>
+            <Button variant="outlined" size="large" onClick={() => (setDisplayMode("draft"),  setDraftBoardAtom(currentBoard), setdraftedBoardAtom({Players: [], Pick: 0}))}>
                 Start Draft
             </Button>
         </>
