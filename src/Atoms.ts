e6d7e0151fb2d18e3cd52036a735e2c1556592ea
TheKLegacy <dominikkingdiepie@gmail.com>
2025-03-28 18:24:43 -0500

    Add get and set for firestore boards

--- src/Atoms.ts
+++ src/Atoms.ts
@@ -1,10 +1,12 @@
 import { type Atom, atom } from "jotai";
 import { initialBoard } from "./InitialData";
 import { atomFamily, atomWithStorage } from "jotai/utils";
-import type { Board, FilterValues } from "./Shared/Filters";
+import type { Board } from "./Shared/Filters";
 import type { Player } from "./player";
 import type { DraftedBoard, SleeperDraft, SleeperPick } from "./types";
+import { writeUserBoards } from "./Firebase/Firestore";
 import { filterPlayers } from "./Shared/utils";
+import { User } from "firebase/auth";
 
 export const currentBoardAtom = atom<String>("Unnamed Board");
 
@@ -22,57 +24,99 @@ export const sleeperDraftAtom = atom<SleeperDraft | null>(null);
 export const sleeperPicksAtom = atom<SleeperPick[]>([]);
 
 export const draftPlayerAction = atom(null, (get, set, payload: Player) => {
-    let board = get(draftBoardAtom);
-    let draftedPlayersBoard = get(draftedBoardAtom);
-    const updatedPlayers = board!.Players.filter(p => p.player_id !== payload.player_id);
-    const updatedDraftedPlayers = [...draftedPlayersBoard.Players, { ...payload, Pick: draftedPlayersBoard.Pick + 1 }];
-    set(draftBoardAtom, { ...board!, Players: updatedPlayers });
-    set(draftedBoardAtom, { Players: updatedDraftedPlayers, Pick: draftedPlayersBoard.Pick + 1 });
-})
+	let board = get(draftBoardAtom);
+	let draftedPlayersBoard = get(draftedBoardAtom);
+	const updatedPlayers = board!.Players.filter(
+		(p) => p.player_id !== payload.player_id
+	);
+	const updatedDraftedPlayers = [
+		...draftedPlayersBoard.Players,
+		{ ...payload, Pick: draftedPlayersBoard.Pick + 1 },
+	];
+	set(draftBoardAtom, { ...board!, Players: updatedPlayers });
+	set(draftedBoardAtom, {
+		Players: updatedDraftedPlayers,
+		Pick: draftedPlayersBoard.Pick + 1,
+	});
+});
 
 export const getCurrentBoardAtom = atom((get) => {
-    const name = get(currentBoardAtom);
-    const board = get(allBoardsAtom);
-    return board.find(b => b.Name === name) ?? {...initialBoard } ;
-})
+	const name = get(currentBoardAtom);
+	const board = get(allBoardsAtom);
+	return board.find((b) => b.Name === name) ?? { ...initialBoard };
+});
 
 export const updateBoardAction = atom(null, (get, set, payload: Board) => {
-    const allBoards = get(allBoardsAtom);
-    const updatedBoards = allBoards.map((board) =>
-        board.Name === payload.Name ? payload : board
-    );
-    set(allBoardsAtom, updatedBoards);
+	const allBoards = get(allBoardsAtom);
+	const updatedBoards = allBoards.map((board) =>
+		board.Name === payload.Name ? payload : board
+	);
+	writeUserBoards(updatedBoards);
+	set(allBoardsAtom, updatedBoards);
 });
 
-export const updateBoardNameAction = atom(null, (get, set, payload: Board & {NewName: string}) => {
-    const allBoards = get(allBoardsAtom);
-    const updatedBoards = allBoards.map((board) =>
-        board.Name === payload.Name ? { ...payload, Name: payload.NewName, NewName: undefined } as Board : board
-    );
+export const updateBoardNameAction = atom(
+	null,
+	(get, set, payload: Board & { NewName: string }) => {
+		const allBoards = get(allBoardsAtom);
+		const updatedBoards = allBoards.map((board) =>
+			board.Name === payload.Name
+				? ({
+						...payload,
+						Name: payload.NewName,
+				  } as Board)
+				: board
+		);
 
-    set(currentBoardAtom, payload.NewName);
-    set(allBoardsAtom, updatedBoards);
-});
+		set(currentBoardAtom, payload.NewName);
+        writeUserBoards(updatedBoards);
+		set(allBoardsAtom, updatedBoards);
+	}
+);
 
 export const deleteCurrentBoardAction = atom(null, (get, set) => {
-    const allBoards = get(allBoardsAtom);
-    const currentBoard = get(currentBoardAtom);
-    const updatedBoards = allBoards.filter((board) => board.Name !== currentBoard);
-    set(allBoardsAtom, updatedBoards);
-    set(currentBoardAtom, updatedBoards[0]?.Name ?? "");
+	const allBoards = get(allBoardsAtom);
+	const currentBoard = get(currentBoardAtom);
+	const updatedBoards = allBoards.filter(
+		(board) => board.Name !== currentBoard
+	);
+    writeUserBoards(updatedBoards);
+	set(allBoardsAtom, updatedBoards);
+	set(currentBoardAtom, updatedBoards[0]?.Name ?? "");
 });
 
 export const currentFiltersAtom = atom((get) => get(getCurrentBoardAtom).Filters);
 export const currentPlayersAtom = atom((get) => get(getCurrentBoardAtom).Players);
 
 export const filteredPlayersAtom = atomFamily(
-    (boardAtom: Atom<Board | undefined>) =>
-      atom((get) => {
-        const board = get(boardAtom);
-        return board ? filterPlayers(board.Players, board.Filters) : [];
-      })
-  );
+	(boardAtom: Atom<Board | undefined>) =>
+		atom((get) => {
+			const board = get(boardAtom);
+			return board ? filterPlayers(board.Players, board.Filters) : [];
+		})
+);
 
-export const allBoardsAtom = atomWithStorage<Board[]>("DraftBoards", [initialBoard]);
+export const allBoardsStorageAtom = atomWithStorage<Board[]>("DraftBoards", [
+	initialBoard,
+]);
+
+export const userAtom = atom<User>();
+
+export const allBoardsUserAtom = atom<Board[]>([]);
+
+export const allBoardsAtom = atom(
+    (get) => {
+      const user = get(userAtom);
+      return user ? get(allBoardsUserAtom) : get(allBoardsStorageAtom);
+    },
+    (get, set, newValue: Board[]) => {
+      const user = get(userAtom); 
+      if (user) {
+        set(allBoardsUserAtom, newValue);
+      } else {
+        set(allBoardsStorageAtom, newValue);
+      }
+    }
+  );
 
 export const cleanedPlayersAtom = atom<Player[]>([]);
