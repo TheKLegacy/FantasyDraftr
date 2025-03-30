22c45796ce5c6324f334b024b2653a1b49e0adef
TheKLegacy <dominikkingdiepie@gmail.com>
2025-03-30 10:07:08 -0500

    cleanup

--- src/Scripts/AllocatePlayersToBoards.ts
+++ src/Scripts/AllocatePlayersToBoards.ts
@@ -3,10 +3,10 @@ import { Board } from "../Shared/Filters";
 import { BoardPayload } from "../types";
 import playerData from "../../data/players.json";
 
-const playerObjects = Object.values(playerData) as unknown as Player[]
+const playerObjects = Object.values(playerData) as unknown as Player[];
 
-export const allocateBoards = (boards: BoardPayload[]): Board[] => boards.map(b => ({...b, Players: allocatePlayers(b.Players)}) as Board);
+export const allocateBoards = (boards: BoardPayload[]): Board[] =>
+	boards.map((b) => ({ ...b, Players: allocatePlayers(b.Players) } as Board));
 
-const allocatePlayers = (players: string[]): Player[] => {
-    return players.map(p => playerObjects.find(o => o.player_id === p)!);
-}
+const allocatePlayers = (players: string[]): Player[] =>
+	players.map((p) => playerObjects.find((o) => o.player_id === p)!);
