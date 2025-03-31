9504d53db942c3d0abf9dd0b0552ce05e5e5b29e
TheKLegacy <dominikkingdiepie@gmail.com>
2025-03-30 22:10:02 -0500

    cleanup login, register, logout flow

--- SleeperParser/GetBestPlayers.js
+++ SleeperParser/GetBestPlayers.js
@@ -1,6 +1,6 @@
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 const propertiesToRemove = [
     "metadata",
@@ -35,17 +35,64 @@ const propertiesToRemove = [
     "practice_description",
     "sportradar_id",
     "practice_participation",
-    "search_rank"
+    "search_rank",
+    "height",
+    "injury_notes",
+    "status",
+    "depth_chart_order",
+    "weight",
+    "number",
+    "active",
+    "years_exp",
+    "fantasy_positions"
+];
+
+const adpPropsToRemove = [
+    "cmp_pct",
+    "fum_lost",
+    "gp",
+    "pass_2pt",
+    "pass_att",
+    "pass_cmp",
+    "pass_fd",
+    "pass_int",
+    "pass_td",
+    "pass_yd",
+    "pts_half_ppr",
+    "pts_ppr",
+    "pts_std",
+    "rush_2pt",
+    "rush_att",
+    "rush_fd",
+    "rush_td",
+    "rush_yd",
+    "bonus_rec_rb",
+    "rec",
+    "rec_0_4",
+    "rec_10_19",
+    "rec_20_29",
+    "rec_30_39",
+    "rec_40p",
+    "rec_5_9",
+    "rec_fd",
+    "rec_td",
+    "rec_yd",
+    "bonus_rec_wr",
+    "rec_2pt",
 ];
 
 async function fetchData() {
     try {
-        const { data: rawPlayerAdpData } = await axios.get('https://api.sleeper.com/projections/nfl/2025?season_type=regular&position[]=DB&position[]=DEF&position[]=DL&position[]=K&position[]=LB&position[]=QB&position[]=RB&position[]=TE&position[]=WR&order_by=adp_dynasty_2qb');
-        let { data: rawPlayerData } = await axios.get('https://api.sleeper.app/v1/players/nfl?limit=2000');
+        const { data: rawPlayerAdpData } = await axios.get(
+            "https://api.sleeper.com/projections/nfl/2025?season_type=regular&position[]=DB&position[]=DEF&position[]=DL&position[]=K&position[]=LB&position[]=QB&position[]=RB&position[]=TE&position[]=WR&order_by=adp_dynasty_2qb"
+        );
+        let { data: rawPlayerData } = await axios.get(
+            "https://api.sleeper.app/v1/players/nfl?limit=2000"
+        );
         rawPlayerData = Object.values(rawPlayerData);
         return { rawPlayerData, rawPlayerAdpData };
     } catch (error) {
-        console.error('Error fetching data from the API:', error);
+        console.error("Error fetching data from the API:", error);
         process.exit(1);
     }
 }
@@ -53,28 +100,48 @@ async function fetchData() {
 async function processData() {
     try {
         const { rawPlayerData, rawPlayerAdpData } = await fetchData();
-        if (!rawPlayerData || !Array.isArray(rawPlayerData) || !rawPlayerAdpData || !Array.isArray(rawPlayerAdpData)) {
+        if (
+            !rawPlayerData ||
+            !Array.isArray(rawPlayerData) ||
+            !rawPlayerAdpData ||
+            !Array.isArray(rawPlayerAdpData)
+        ) {
             console.log("No valid data found in the files.");
             return;
         }
 
         const sortedPlayerData = rawPlayerData
-            .map(d => {
-                var adp = rawPlayerAdpData.find(dp => dp.player_id == d.player_id)?.stats;
-                propertiesToRemove.forEach((property) => {
-                    d[property] = undefined; // Remove each specified property
+            .map((d) => {
+                var adp = rawPlayerAdpData.find(
+                    (dp) => dp.player_id == d.player_id
+                )?.stats;
+                adpPropsToRemove.forEach((property) => {
+                    adp && (adp[property] = undefined);
                 });
-                return { ...d,  adpStats: adp };
+                propertiesToRemove.forEach((property) => {
+                    d[property] = undefined;
+                });
+                return { ...d, adpStats: adp };
             })
-            .filter(d => d.adpStats?.adp_dynasty_2qb && ["QB","WR","TE","RB"].includes(d.position)) 
-            .sort((a, b) => a.adpStats.adp_dynasty_2qb - b.adpStats.adp_dynasty_2qb)
-            .slice(0, 1000);
+            .filter(
+                (d) =>
+                    d.adpStats?.adp_dynasty_2qb &&
+                    ["QB", "WR", "TE", "RB"].includes(d.position)
+            )
+            .sort(
+                (a, b) =>
+                    a.adpStats.adp_dynasty_2qb - b.adpStats.adp_dynasty_2qb
+            )
+            .slice(0, 500);
 
-        const outputFilePath = path.join("../data/", 'players.json');
-        fs.writeFileSync(outputFilePath, JSON.stringify(sortedPlayerData, null, 2));
-        console.log("Top 1000 entries have been saved to", outputFilePath);
+        const outputFilePath = path.join("../data/", "players.json");
+        fs.writeFileSync(
+            outputFilePath,
+            JSON.stringify(sortedPlayerData, null, 2)
+        );
+        console.log("Top 500 entries have been saved to", outputFilePath);
     } catch (error) {
-        console.error('Error processing or writing data:', error);
+        console.error("Error processing or writing data:", error);
     }
 }
 
